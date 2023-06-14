db30bd3a009b4c85fe9acd52c0eb94b4adb1f4a6
NemuiNekoRoK <hsjee9701@gmial.com>
2023-06-14 11:48:53 +0000

    update

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
 import discord
 import datetime
+from datetime import datetime, timezone, timedelta
+from pytz import timezone
 import requests
 from discord.ext import commands
 from bs4 import BeautifulSoup
 import asyncio
 
 #환경변수
-TOKEN = "MTExNTY5NDc0NDY4OTk3MTI5Mg.Gip70Q.AzyS3U_Ik4bIdWgZprkiksKiYfPX6temSkmT5w"
+TOKEN = "MTExNTY5NDc0NDY4OTk3MTI5Mg.GQWWYg.Dl1789waz61VwLkY9VK-ieWkw9Y-fxHeTJnwg0"
 GUILD_ID = 1115695314955931679
 CHANNEL_ID = "notice"
 MAPLESTORY_URL = "https://maplestory.nexon.com/News/Notice"
-URS_START_HOUR = 13
-URS_END_HOUR = 23
+URS_START_HOUR = 4 #utc 기준
+URS_END_HOUR = 13
 player_records = {}
 
 intents = discord.Intents.default()
@@ -19,11 +21,12 @@ intents.typing = False
 intents.presences = False
 intents.messages = True
 intents.message_content = True  # message_content intent 추가
-
 bot = commands.Bot(command_prefix="~", intents=intents, case_insensitive=True)
 
 #현재시간
-now = datetime.datetime.now()
+KST = timezone('Asia/Seoul')
+now = datetime.utcnow().astimezone(KST)
+
 
 @bot.event
 async def on_ready(): #봇 준비 명령어
@@ -32,7 +35,7 @@ async def on_ready(): #봇 준비 명령어
     await bot.change_presence(activity=discord.Game("~도움말로 명령어를 알려드려요!"), status=discord.Status.dnd)
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
-    await channel.send(f"테스트트트트")
+    await channel.send(f"Init! Bot is Started!")
     #await schedule_tasks()#스케줄 태스크 시작
     bot.loop.create_task(schedule_tasks())
     bot.loop.create_task(schedule_tasks2())
@@ -42,29 +45,25 @@ async def on_ready(): #봇 준비 명령어
 #스케줄 시작
 async def schedule_tasks():
     print("start")
-    urs_start_time = datetime.datetime(now.year, now.month, now.day, URS_START_HOUR) #각종 변수 세팅
-    urs_end_time = datetime.datetime(now.year, now.month, now.day, URS_END_HOUR)
+    urs_start_time = datetime(now.year, now.month, now.day, URS_START_HOUR).astimezone(KST) #각종 변수 세팅
+    urs_end_time = datetime(now.year, now.month, now.day, URS_END_HOUR).astimezone(KST)
     if now.hour >= URS_END_HOUR:  # 현재 시간이 URS_END_HOUR 이후라면 다음 날로 설정
-        urs_start_time += datetime.timedelta(days=1)
-        urs_end_time += datetime.timedelta(days=1)
+        urs_start_time += timedelta(days=1)
+        urs_end_time += timedelta(days=1)
     time_until_urs_start = urs_start_time - now
+    
     seconds_until_urs_start = int(time_until_urs_start.total_seconds())
 
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
     #time = datetime.datetime.now()
-    #await channel.send(f"탸임스탬프 {time}")
-
-    await test(10)
-
+    stemp = f"{now.strftime('%Y-%m-%d %X')}"
+    await channel.send(f"최초타임스탬프 {stemp}")
+    await channel.send("Task1 Urs")
+    await channel.send(f"우르스 시작시간 {urs_start_time.strftime('%Y-%m-%d %X')}")
+    await channel.send(f"우르스 종료시간 {urs_end_time.strftime('%Y-%m-%d %X')}")
     await urs_start_task(seconds_until_urs_start)
     await urs_end_task()
-
-async def test(delay):
-    await asyncio.sleep(delay)
-    guild = bot.get_guild(GUILD_ID)
-    channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
-    await channel.send(f"딜레이")
 
 #우르스 시작
 async def urs_start_task(seconds_until_urs_start):
@@ -73,19 +72,15 @@ async def urs_start_task(seconds_until_urs_start):
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
     #time = datetime.datetime.now()
-    # await channel.send(f"탸임스탬프 {time}")
+   # await channel.send(f"탸임스탬프 {time}")
     await channel.send(f"우르스 2배 이벤트 시작했어! 메소 벌러 가야지~")
 
     await urs_end_task()
 
-
-
-
-
 async def urs_end_task():#우르스 끝나는 데스크
-    urs_end_time = datetime.datetime(now.year, now.month, now.day, URS_END_HOUR)
+    urs_end_time = datetime(now.year, now.month, now.day, URS_END_HOUR)
     if now.hour >= URS_END_HOUR:  # 현재 시간이 URS_END_HOUR 이후라면 다음 날로 설정
-        urs_end_time += datetime.timedelta(days=1)
+        urs_end_time += timedelta(days=1)
     time_until_urs_end = urs_end_time - now
     seconds_until_urs_end = int(time_until_urs_end.total_seconds())
     await asyncio.sleep(seconds_until_urs_end)
@@ -93,29 +88,32 @@ async def urs_end_task():#우르스 끝나는 데스크
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
     await channel.send(f"우르스 2배 이벤트 끝났어.. 다음을 기약하자!")
 
+
 async def schedule_tasks3():
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
-    time = datetime.datetime.now()
-    await channel.send(f"타임스탬프 시작{time}")
+    time = datetime.utcnow().astimezone(KST)
+    
+    await channel.send("Task3 Timestamp")
+    await channel.send(f"타임스탬프 시작{time.strftime('%Y-%m-%d %X')}")
     await timeStamp(3600)
 
-async def timeStamp(x):
-    await asyncio.sleep(x)
+async def timeStamp(time):
+    await asyncio.sleep(time)
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
-    time = datetime.datetime.now()
-    await channel.send(f"디버깅 타임스탬프 {time}")
-
+    t = datetime.utcnow().astimezone(KST)
+    await channel.send(f"디버깅 타임스탬프 {t.strftime('%Y-%m-%d %X')}")
+    await timeStamp(time)
 async def schedule_tasks2():
     bot.loop.create_task(maple_task())
 
     guild = bot.get_guild(GUILD_ID)
     channel = discord.utils.get(guild.channels, name=CHANNEL_ID)
-
-    next_urs_start_time = datetime.datetime(now.year, now.month, now.day, URS_START_HOUR)
+    await channel.send("Task2 Notice")
+    next_urs_start_time = datetime(now.year, now.month, now.day, URS_START_HOUR).astimezone(KST)
     if now > next_urs_start_time:
-        next_urs_start_time += datetime.timedelta(days=1)
+        next_urs_start_time += timedelta(days=1)
     time_until_urs_start = next_urs_start_time - now
     seconds_until_urs_start = int(time_until_urs_start.total_seconds())
 
